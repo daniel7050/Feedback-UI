8a2d9008683a5b609f0b44817c6ba5874879f198
Omole Daniel <danielomole18@gmail.com>
2023-03-03 22:32:05 +0100

    i created components for the feedback UI  and edited the files that  came with installing react

--- Feedback-app/src/App.jsx
+++ Feedback-app/src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackData from "./Data/FeedbackData"
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+    const [feedback, setFeedback] = useState(FeedbackData)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item) => item.id !==id ))
+        }
+    }
+
+
+    return <>
+        <Header/>
+        <div className="container">
+            <FeedbackStats feedback= {feedback} />
+            <FeedbackList feedback= {feedback} handleDelete= {deleteFeedback}/>
+        </div>
+    </>
+
 }
+
 
 export default App
